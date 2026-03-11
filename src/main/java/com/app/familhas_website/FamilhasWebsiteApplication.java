1303a8c3a03831f48412b09989f974cb78e0d221
guilherme.ferreira <guiguiferreiraw@gmail.com>
2026-03-10 21:34:21 -0300

    feat: iniciando projeto

---
+++ src/main/java/com/app/familhas_website/FamilhasWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.app.familhas_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilhasWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilhasWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/familhas_website/FamilhasWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.familhas_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilhasWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
